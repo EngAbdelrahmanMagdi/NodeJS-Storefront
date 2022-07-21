27b0e075ad4a0edf476f9c262bab9bb1526fd208
EngAbdelrahmanMagdi <eng.abdelrahman.magdi@gmail.com>
2022-07-22 01:40:02 +0200

    configuation for prettier added

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const serverPort = process.env.PORT || 3000;
-const app: Application = express()
-
-
+const app: Application = express();
 
 app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(3000, function () {
-    console.log(`The server is running in Port ${serverPort}`)
-})
+  console.log(`The server is running in Port ${serverPort}`);
+});
