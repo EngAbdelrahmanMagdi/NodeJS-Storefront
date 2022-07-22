78cddf62e334c220f4e8c353acc0c7b6b22cbf14
EngAbdelrahmanMagdi <eng.abdelrahman.magdi@gmail.com>
2022-07-22 03:55:08 +0200

    database tests configuation

--- src/database.ts
+++ src/database.ts
@@ -2,13 +2,20 @@ import dotenv from 'dotenv';
 import { Pool } from 'pg';
 
 dotenv.config();
-const { HOST, DEV_DB, USER, PASSWORD } = process.env;
+const { HOST, DEV_DB, USER, PASSWORD, TEST_DB, ENV } = process.env;
 
-const Database = new Pool({
-  host: HOST,
-  database: DEV_DB,
-  user: USER,
-  password: PASSWORD,
-});
-
+const Database =
+  ENV === 'dev'
+    ? new Pool({
+        host: HOST,
+        database: DEV_DB,
+        user: USER,
+        password: PASSWORD,
+      })
+    : new Pool({
+        host: HOST,
+        database: TEST_DB,
+        user: USER,
+        password: PASSWORD,
+      });
 export default Database;
